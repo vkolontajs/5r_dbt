e6b4048374ce7937316cf6fbf2cb30e5e94b6215
Vladislavs Kolontajs <qorqa.info@gmail.com>
2022-11-26 13:53:21 +0000

    aa

--- models/marts/campaigns.sql
+++ models/marts/campaigns.sql
@@ -1,4 +1,17 @@
+with dirty_campaings as (
+
 select
-    *
+    cast(campaign_name as string) as campaign_name
 from
     {{ ref('fb_ads_raw') }}
+
+union all
+
+select
+    cast(campaign as string) as campaign_name
+from
+    {{ ref('ga_campaigns_raw') }}
+
+)
+
+select * from dirty_campaings
